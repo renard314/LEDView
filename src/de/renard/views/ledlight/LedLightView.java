414372da6d5877ca442655e3988c998cd86b54bb
Renard Wellnitz <Renard.Wellnitz@googlemail.com>
2012-04-15 19:34:20 +0200

    added some comments

--- src/de/renard/views/ledlight/LedLightView.java
+++ src/de/renard/views/ledlight/LedLightView.java
@@ -23,34 +23,30 @@ import com.nineoldandroids.animation.Animator.AnimatorListener;
 import com.nineoldandroids.animation.ObjectAnimator;
 
 public class LEDLightView extends View implements Checkable {
-	private RectF mBounds = new RectF();
-	private RectF mInnerBounds = new RectF();
-	private RectF mGlowBounds = new RectF();
-	private final Paint mRimPaint;
-	private final Paint mRimCirclePaint;
-	private final Paint mRimShadowPaint;
-	private final Paint mGlowPaint;
-	private final Paint mGlassPaint;
-	private ObjectAnimator mAnimator;
 
+	private RectF mBounds = new RectF(); // bounds for round led light
+	private RectF mInnerBounds = new RectF(); // bounds of glass dome
+	private RectF mGlowBounds = new RectF(); // bounds for glow
+	private final Paint mRimPaint; // metallic background
+	private final Paint mRimCirclePaint; // border of metallic background
+	private final Paint mRimShadowPaint; // inset shadow of metallic background
+	private final Paint mGlowPaint; // light source
+	private final Paint mGlassPaint;// glass dome above the light TODO make it
+									// more look like glass
+	private ObjectAnimator mAnimator; // animates between on and off state
+
+	//pre allocated arrays
 	final int[] glassGradientColors = new int[5];
 	final int[] glowGradientColors = new int[3];
 	final float[] glassGradientPositions = new float[5];
 	final float[] glowGradientPositions = new float[3];
 	final float[] hsv = new float[3];
-	float mLightIntensity = 0;
+	float mLightIntensity = 0; // current intensity of the light. range [0-1]
 	private boolean mOn = false;
 	int mLightColor;
 	private boolean mRecalculateGlowShader;
-	private Bitmap mDrawingCache = null;
+	private Bitmap mBackground = null;
 
-	private void init(AttributeSet attrs) {
-
-		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.LEDLightView);
-		final int color = a.getColor(R.styleable.LEDLightView_LightColor, Color.GREEN);
-		setLightColor(color);
-		a.recycle();
-	}
 
 	public LEDLightView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -80,29 +76,47 @@ public class LEDLightView extends View implements Checkable {
 		};
 		initGlassColors();
 		initGlowColors();
-
+	}
+	
+	private void init(AttributeSet attrs) {
+		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.LEDLightView);
+		final int color = a.getColor(R.styleable.LEDLightView_LightColor, Color.GREEN);
+		setLightColor(color);
+		a.recycle();
 	}
 
+
+	/**
+	 * the glow is made up of a radial gradient dependant on the current light intensity
+	 */
 	private void initGlowColors() {
+		//outer edges fade out
 		final int i1 = (int) Math.min(255, 0 * mLightIntensity);
 		final int i2 = (int) Math.min(255, 155 * mLightIntensity);
 		final int i3 = (int) Math.min(255, 255 * mLightIntensity);
+		//convert to hsv to change intensity
 		Color.colorToHSV(mLightColor, hsv);
 		hsv[2] = mLightIntensity;
+		//convert back to argb
 		final int color = Color.HSVToColor(hsv);
 		final int r = Color.red(color);
 		final int g = Color.green(color);
 		final int b = Color.blue(color);
 		glowGradientColors[2] = Color.argb(i1, r, g, b);
 		glowGradientColors[1] = Color.argb(i2, r, g, b);
-		// hsv[1] = 1 - 0.4f * mLightIntensity;
+		hsv[1] -= hsv[1]* 0.25f * mLightIntensity; //make the center of the light a litte bit white
 		glowGradientColors[0] = Color.HSVToColor(i3, hsv);
 		glowGradientPositions[2] = 0.90f;
 		glowGradientPositions[1] = 0.70f;
 		glowGradientPositions[0] = 0.25f;
+		//next onDraw() will create a new glow shader
 		mRecalculateGlowShader = true;
 	}
 
+	/**
+	 * glass dome is emulated by a radial gradient
+	 * glass has a light tint of the light color
+	 */
 	private void initGlassColors() {
 		Color.colorToHSV(mLightColor, hsv);
 		hsv[2] = 0.27f;
@@ -135,23 +149,24 @@ public class LEDLightView extends View implements Checkable {
 			final float o = 0.02f * size;
 
 			mBounds.set(l, t, l + size, t + size);
-			float inset = size * 0.15f;
+			float inset = size * 0.15f; //led is 15% smaller than the bounds so that the glow of the light can be larger than the led
 			mBounds.inset(inset, inset);
 			mInnerBounds.set(mBounds.left + o, mBounds.top + o, mBounds.right - o, mBounds.bottom - o);
 			mGlowBounds.set(mInnerBounds);
-			mGlowBounds.inset(-inset, -inset);
+			mGlowBounds.inset(-inset, -inset); //glow bounds take up all space
 			final float innerSize = mInnerBounds.width();
 
-			mRimPaint.setShader(new LinearGradient(0.40f * size + mBounds.left, 0.0f + mBounds.top, 0.60f * size + mBounds.left, mBounds.bottom, Color.rgb(0x80, 0x85, 0x80), Color.rgb(0x30, 0x31, 0x30), Shader.TileMode.CLAMP));
+			final float insetSize = size-2*inset;
+			mRimPaint.setShader(new LinearGradient(0.40f * insetSize + mBounds.left, 0.0f + mBounds.top, 0.60f * insetSize + mBounds.left, mBounds.bottom, Color.rgb(0x80, 0x85, 0x80), Color.rgb(0x30, 0x31, 0x30), Shader.TileMode.CLAMP));
 
-			mRimCirclePaint.setStrokeWidth(0.005f * size);
+			mRimCirclePaint.setStrokeWidth(0.005f * insetSize);
 
 			mRimShadowPaint.setShader(new RadialGradient(mInnerBounds.centerX(), mInnerBounds.centerY(), innerSize / 2, new int[] { 0x00000000, 0x00000500, 0x50000500 }, new float[] { 0.96f, 0.96f, 0.99f }, Shader.TileMode.MIRROR));
 			mRimShadowPaint.setStyle(Paint.Style.FILL);
 
 			RadialGradient glassShader = new RadialGradient(mBounds.centerX(), mBounds.centerY(), (int) mBounds.height(), glassGradientColors, glassGradientPositions, TileMode.CLAMP);
 			mGlassPaint.setShader(glassShader);
-			mGlassPaint.setXfermode(new PorterDuffXfermode(Mode.ADD));
+			mGlassPaint.setXfermode(new PorterDuffXfermode(Mode.ADD)); //glass is added on top of light
 
 			createDrawingCache(width, height);
 
@@ -159,18 +174,18 @@ public class LEDLightView extends View implements Checkable {
 	}
 
 	/**
-	 * create a bitmap containing the background of the LED
+	 * draw background of the led into a bitmap
 	 * 
 	 * @param width
 	 * @param height
 	 */
 	private void createDrawingCache(final int width, final int height) {
-		if (mDrawingCache != null) {
-			mDrawingCache.recycle();
+		if (mBackground != null) {
+			mBackground.recycle();
 		}
 
-		mDrawingCache = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-		Canvas c = new Canvas(mDrawingCache);
+		mBackground = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		Canvas c = new Canvas(mBackground);
 		c.drawOval(mBounds, mRimPaint);
 		c.drawOval(mBounds, mRimCirclePaint);
 		c.drawOval(mInnerBounds, mRimCirclePaint);
@@ -179,12 +194,13 @@ public class LEDLightView extends View implements Checkable {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		//eclipse cant handle this view
 		if (isInEditMode()) {
 			canvas.drawOval(mBounds, mRimPaint);
 			return;
 		}
 
-		canvas.drawBitmap(mDrawingCache, 0, 0, null);
+		canvas.drawBitmap(mBackground, 0, 0, null);
 
 		if (mRecalculateGlowShader) {
 			final float radius = mGlowBounds.height() / 2;
@@ -194,13 +210,12 @@ public class LEDLightView extends View implements Checkable {
 		}
 		canvas.drawOval(mGlowBounds, mGlowPaint);
 		canvas.drawOval(mInnerBounds, mGlassPaint);
-		canvas.restore();
 	}
 
 	/**
 	 * animates the light value to desired target [0-1].
 	 */
-	public void animateLightIntensity(final float target) {
+	public void animateLightIntensity(final float target) {		
 		if (mAnimator != null && mAnimator.isRunning()) {
 			mAnimator.addListener(new AnimatorListener() {
 
@@ -230,18 +245,21 @@ public class LEDLightView extends View implements Checkable {
 		}
 	}
 
-	public void setLightColor(final int color) {
-		mLightColor = color;
-		initGlassColors();
-	}
+
 
 	private void startAnimation(final float target) {
 		mAnimator = ObjectAnimator.ofFloat(this, "lightIntensity", mLightIntensity, target);
 		mAnimator.setInterpolator(new DecelerateInterpolator());
-		mAnimator.setDuration(150);
+		final float scale = Math.abs(target - mLightIntensity);
+		mAnimator.setDuration((long) (150 * scale));
 		mAnimator.start();
 	}
 
+	public void setLightColor(final int color) {
+		mLightColor = color;
+		initGlassColors();
+	}
+	
 	/**
 	 * instantly sets the light to desired intensity. does not animate.
 	 * 
@@ -266,7 +284,7 @@ public class LEDLightView extends View implements Checkable {
 
 	@Override
 	public boolean isChecked() {
-		return mLightIntensity == 1;
+		return mOn ;
 	}
 
 	@Override
